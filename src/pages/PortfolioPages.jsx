5d66c6dee22aa70f1a63775d13ad75882abd2d62
jose.deharo.intern <josedeharojimenez1@gmail.com>
2025-11-09 12:19:40 +0100

    laws

--- src/pages/PortfolioPages.jsx
+++ src/pages/PortfolioPages.jsx
@@ -1,37 +1,45 @@
 import React from "react";
 /*Importación de las hojas de estilos */
-import './PortfolioPages.css';
+import "./PortfolioPages.css";
 /*-------------------------------------- */
 /*Importación de los componenentes */
-import Hero from '../components/heroComponent/Hero.jsx';
-import SectionCard from '../components/Card/Card.jsx';
-import Element3D from '../components/3d/Element3d.jsx';
-import Background from '../components/Background/Background.jsx';
+import Hero from "../components/heroComponent/Hero.jsx";
+import SectionCard from "../components/Card/Card.jsx";
+import Element3D from "../components/3d/Element3d.jsx";
+import Background from "../components/Background/Background.jsx";
 /*-------------------------------------- */
 
 /*Importación de información en json */
-import redesSociales from '../data/redesSociales.json';
-import infoPersonalData from '../data/personalDataHero.json';
-import infoWebsData from '../data/webDesign.json';
+import redesSociales from "../data/redesSociales.json";
+import infoPersonalData from "../data/personalDataHero.json";
+import infoWebsData from "../data/webDesign.json";
 /*-------------------------------------- */
 /*Importación de información  */
-import{ arrLenguajeDeMarca, arrLenguajeDeProgramacion, arrFrameWorks } from '../../public/assets/import.js'; 
+import {
+  arrLenguajeDeMarca,
+  arrLenguajeDeProgramacion,
+  arrFrameWorks,
+} from "../../public/assets/import.js";
 const [html, css] = arrLenguajeDeMarca;
 const [js] = arrLenguajeDeProgramacion;
 const [angular] = arrFrameWorks;
 /*-------------------------------------- */
 
-const Portfolio =   () =>{
+const Portfolio = () => {
   return (
     <>
-         <Hero titulo={infoPersonalData.nombre}  subtitulo={infoPersonalData.subtitulo} images={[ html,css , js, angular]} descripcion={infoPersonalData.descripcion} tecnologiasInfo={redesSociales} bool={true}></Hero>
-         <SectionCard infoCard={infoWebsData}></SectionCard>
-         <Element3D tip="cubo"></Element3D>
-         <Background></Background>
-        <Hero titulo={"pepe"}  subtitulo={"jkwnejnw"} images={[ html,css , js, angular]} descripcion={infoPersonalData.descripcion} tecnologiasInfo={redesSociales} bool={true}></Hero>
+      <Hero
+        titulo={infoPersonalData.nombre}
+        subtitulo={infoPersonalData.subtitulo}
+        images={[html, css, js, angular]}
+        descripcion={infoPersonalData.descripcion}
+        tecnologiasInfo={redesSociales}
+        bool={true}
+      ></Hero>
+      <SectionCard infoCard={infoWebsData}></SectionCard>
+      <Element3D tip="cubo"></Element3D>
     </>
-   
   );
-}
+};
 
 export default Portfolio;
